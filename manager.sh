dd74cdbb394a6e434b0cd2c8994d2174230ec1fe
Lukas Hamm <ideallygrey@tuta.io>
2022-02-05 23:08:44 -0500

    Update manager.sh

--- manager.sh
+++ manager.sh
@@ -1,4 +1,16 @@
+responce = "0"
+
+printf -- "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
 printf -- "\nLoading...\n"
-printf -- "Java version: "
+printf -- "Java version: \n----------\n"
 java --version
+printf -- "----------\n\n\n"
+printf -- "---Minecraft Server Manager---\n"
+printf -- "\n1 - View Servers\n"
+printf -- "2 - Create New Server\n"
+printf -- "3 - Exit\n"
+printf -- ">> "
+read -r responce
+printf -- "You selected: "
+printf -- "$responce"
 printf -- "\n"

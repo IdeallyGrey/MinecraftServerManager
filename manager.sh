e90f777cb5352808fc27e1a25639312a0e3ed66e
Lukas Hamm <ideallygrey@tuta.io>
2022-02-07 14:42:02 -0500

    Update manager.sh

--- manager.sh
+++ manager.sh
@@ -1,16 +1,28 @@
-responce="0"
 
-printf -- "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-printf -- "\nLoading...\n"
-printf -- "Java version: \n----------\n"
-java --version
-printf -- "----------\n\n\n"
-printf -- "---Minecraft Server Manager---\n"
-printf -- "\n1 - View Servers\n"
-printf -- "2 - Create New Server\n"
-printf -- "3 - Exit\n"
-printf -- ">> "
-read -r responce
-printf -- "You selected: "
-printf -- "$responce"
-printf -- "\n"
+lock="1"
+while [ "$lock" = "1" ];
+do
+	lock="0"
+	responce="0"
+	printf -- "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+	printf -- "\nLoading...\n"
+	printf --  "Java version: \n----------\n"
+	java --version
+ 	printf -- "----------\n\n\n"
+ 	printf -- "---Minecraft Server Manager---\n"
+ 	printf -- "\n1 - View Servers\n"
+ 	printf -- "2 - Create New Server\n"
+ 	printf -- "3 - Exit\n"
+	printf -- ">> "
+	read -r responce
+	case $responce in
+	1)
+	printf -- "View Servers\n" ;;
+	2)
+	printf -- "Create New Server\n" ;;
+	3)
+	printf -- "Goodbye!\n" && exit 0 ;;
+	*)
+	printf -- "That's not a valid number!\n" && lock="1" && sleep 3 ;;	
+	esac
+done
